fef1bc37e0ebe75de75b57b90ff1520bcc514688
Sall, Ian <iansall@hotmail.com>
2021-11-19 16:08:02 +0000

    Reorged project structure, added tests project.

---
+++ CompanyX.Promotions/Promotions.Tests/PromoEngineTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CompanyX.Promotions.Tests
+{
+    public class PromoEngineTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
